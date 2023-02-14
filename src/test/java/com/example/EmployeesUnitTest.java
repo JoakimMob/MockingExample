868f5313fbcc93b09fa2524e91c87fc97cb82138
joakim <joakimmoberg@hotmail.se>
2023-02-14 13:25:53 +0100

    implemented tests for mock

--- src/test/java/com/example/EmployeesUnitTest.java
+++ src/test/java/com/example/EmployeesUnitTest.java
@@ -1,7 +1,39 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class EmployeesUnitTest {
 
+    EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+    BankService bankService = mock(BankService.class);
+
+    private final Employees employees = new Employees(employeeRepository, bankService);
+
+    Employee employee = mock(Employee.class);
+
+
+    @Test
+    void testIfOneEmployee(){
+        when(employeeRepository.findAll()).thenReturn(List.of(new Employee("1",4000)));
+        int numberOfEmployees = new Employees(employeeRepository,bankService).payEmployees();
+        assertEquals(1,numberOfEmployees);
+    }
+    @Test
+    void payEmployees(){
+        when(employeeRepository.findAll()).thenReturn(List.of(employee));
+        when(employeeRepository.save(any(Employee.class))).then(returnsFirstArg());
+        assertEquals(1, employees.payEmployees());
+        verify(employee, times(1)).getSalary();
+        verify(employee, times(1)).setPaid(true);
+    }
 }
